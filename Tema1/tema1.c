976647c951fa3a794a6a5dd2db51a1e2a72d1a9b
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-03-07 19:51:47 +0200

    [Tema_1] Implementat functie parsare

--- Tema1/tema1.c
+++ Tema1/tema1.c
@@ -18,62 +18,142 @@
 
 #define BUFFSIZE 20000
 
- void parse_command(char* buffer, Hashtable *hash){
- 	return;
- }
 
- Hashtable* hash;
- char buff[BUFFSIZE];
+void parse_command(char* buffer, Hashtable *hash){
 
- int main(int argc,char* argv[]){
+	int res;
 
- 	int i;
- 	FILE *f;
- 	DIE(argc<2,"Invalid Input");
+	char* token = strtok(buffer, " ");
+	char* aux;
+	/* Parse add */
+	if(strcmp(token,"add") == 0){
+		token = strtok(NULL, " ");
+		res = Hash_add(token,hash);
+		DIE(res<0,"Error in Hash_add");
+		return;
+	}
+
+	/* Parse remove */
+	if(strcmp(token,"remove") == 0){
+		token = strtok(NULL, " ");
+		res = Hash_remove(token,hash);
+		DIE(res<0,"Error in Hash_remove");
+		return;
+	}
+
+	/* Parse clear */
+	if(strcmp(token,"clear\n") == 0){
+		res = Hash_clear(hash);
+		DIE(res<0,"Error in Hash_clear");
+		return;
+	}		
+	/* Parse find */
+	if(strcmp(token,"find") == 0){
+		token = strtok(NULL, " ");
+		aux = strtok(NULL, " ");
+		/* Check if file is an option */
+		if(aux != NULL){
+			res = Hash_find(token,aux,hash);
+			DIE(res<0,"Error in Hash_find");	
+		}else{
+			res = Hash_find(token,NULL,hash);
+			DIE(res<0,"Error in Hash_find");	
+		}
+		return;
+	}
+	/* Parse print_bucket */
+	if(strcmp(token,"print_bucket") == 0){
+		token = strtok(NULL, " ");
+		uint32_t index = atoi(token);
+		aux = strtok(NULL, " ");
+		/* Check if file is an option */
+		if(aux != NULL){
+			res = Hash_print_bucket(index,aux,hash);
+			DIE(res<0,"Error in Hash_print_bucket");	
+		}else{
+			res = Hash_print_bucket(index,NULL,hash);
+			DIE(res<0,"Error in Hash_print_bucket");	
+		}
+		return;
+	}
+	/* Parse print */
+	if(strcmp(token,"print") == 0){
+		aux = strtok(NULL, " ");
+		/* Check if file is an option */
+		if(aux != NULL){
+			res = Hash_print(aux,hash);
+			DIE(res<0,"Error in Hash_print");	
+		}else{
+			res = Hash_print(NULL,hash);
+			DIE(res<0,"Error in Hash_print");	
+		}
+		return;
+	}
+	/* Parse resize */
+	if(strcmp(token,"resize") == 0){
+		token = strtok(NULL, " ");
+		if(strcmp(token,"double\n") == 0){
+			res = Hash_resize_double(hash);
+			DIE(res<0,"Error in Hash_resize_double");	
+		}
+		if(strcmp(token,"halve\n") == 0){
+			res = Hash_resize_halve(hash);
+			DIE(res<0,"Error in Hash_resize_halve");	
+		}
+		return;
+	}
+	return;
+}
+
+Hashtable* hash;
+char buff[BUFFSIZE];
+
+int main(int argc,char* argv[]){
+
+	int i;
+	FILE *f;
+	DIE(argc<2,"Invalid Input");
 
 	/* Set hashtable_size */
- 	uint32_t hashtable_size = atoi(argv[1]);
+	uint32_t hashtable_size = atoi(argv[1]);
 
 	/* Allocate Hashtable */
- 	hash = create_Hashtable(hashtable_size);
- 	DIE(hash==NULL,"Unable to allocate");
+	hash = create_Hashtable(hashtable_size);
+	DIE(hash==NULL,"Unable to allocate");
 
- 	if(argc <= 2){
- 		while(!feof(stdin)){
-			/* Read Command */
- 			fgets(buff,BUFFSIZE,stdin);
+	/* Parse from STDIN */
+	if(argc <= 2){
+		
+		/* Read Command */
+		fgets(buff,BUFFSIZE,stdin);
 
+		while(!feof(stdin)){
 			/* Parse Command */
- 			parse_command(buff,hash);
+			parse_command(buff,hash);
 
-			/* check if emppy line */
- 			if(strcmp(buff,"\n")!=0){
- 				dprintf("Parsez\n");
- 			}
- 		}
- 	}else{
- 		for(i = 2 ; i < argc ; i++){
+			/* Read Command */
+			fgets(buff,BUFFSIZE,stdin);
+		}
+	/* Parse from FILES */
+	}else{
+		for(i = 2 ; i < argc ; i++){
 			/* Open file */
- 			f = fopen(argv[i], "r");
- 			DIE(f == NULL , "Unable to open File");
+			f = fopen(argv[i], "r");
+			DIE(f == NULL , "Unable to open File");
 
+			/* Read Command */
+			fgets(buff,BUFFSIZE,f);
 
- 			while(!feof(f)){
+			while(!feof(f)){
+				/* Parse Command */
+				parse_command(buff,hash);
 
 				/* Read Command */
- 				fgets(buff,BUFFSIZE,f);
-
-				/* Parse Command */
- 				parse_command(buff,hash);
-
-				/* check if emppy line */
- 				if(strcmp(buff,"\n")!=0){
- 					dprintf("Parsez\n");
- 				}
- 			}
+				fgets(buff,BUFFSIZE,f);
+			}
 			/* Close file */
- 			fclose(f);
- 		}
- 	}
- 	return 0;
- }
+			fclose(f);
+		}
+	}
+	return 0;
+}
