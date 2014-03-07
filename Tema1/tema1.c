89a2110cf4448c24ffefcb4d112b0712677f135b
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-03-07 18:14:27 +0200

    [Tema_1] Implementat partea de input

--- Tema1/tema1.c
+++ Tema1/tema1.c
@@ -8,17 +8,72 @@
  *
  */
 
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "debug.h"
 #include "utils.h"
+#include "functii.h"
 
-int main(void){
+#define BUFFSIZE 20000
+
+ void parse_command(char* buffer, Hashtable *hash){
+ 	return;
+ }
+
+ Hashtable* hash;
+ char buff[BUFFSIZE];
+
+ int main(int argc,char* argv[]){
+
+ 	int i;
+ 	FILE *f;
+ 	DIE(argc<2,"Invalid Input");
+
+	/* Set hashtable_size */
+ 	uint32_t hashtable_size = atoi(argv[1]);
+
+	/* Allocate Hashtable */
+ 	hash = create_Hashtable(hashtable_size);
+ 	DIE(hash==NULL,"Unable to allocate");
+
+ 	if(argc <= 2){
+ 		while(!feof(stdin)){
+			/* Read Command */
+ 			fgets(buff,BUFFSIZE,stdin);
+
+			/* Parse Command */
+ 			parse_command(buff,hash);
+
+			/* check if emppy line */
+ 			if(strcmp(buff,"\n")!=0){
+ 				dprintf("Parsez\n");
+ 			}
+ 		}
+ 	}else{
+ 		for(i = 2 ; i < argc ; i++){
+			/* Open file */
+ 			f = fopen(argv[i], "r");
+ 			DIE(f == NULL , "Unable to open File");
 
 
-	dprintf("Nanu e tare");
+ 			while(!feof(f)){
 
-	DIE(1,"asta este fals");
+				/* Read Command */
+ 				fgets(buff,BUFFSIZE,f);
 
-	return 0;
-}
+				/* Parse Command */
+ 				parse_command(buff,hash);
+
+				/* check if emppy line */
+ 				if(strcmp(buff,"\n")!=0){
+ 					dprintf("Parsez\n");
+ 				}
+ 			}
+			/* Close file */
+ 			fclose(f);
+ 		}
+ 	}
+ 	return 0;
+ }
