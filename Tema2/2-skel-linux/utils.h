252a75b343cfb02d90fce21a42793642fcc2671a
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-03-29 21:39:09 +0200

    [Tema_2] Adaugat macroul DIE in utils.h

--- Tema2/2-skel-linux/utils.h
+++ Tema2/2-skel-linux/utils.h
@@ -13,6 +13,20 @@
 
 #define SHELL_EXIT -100
 
+ /* useful macro for handling error codes */
+#define DIE(assertion, call_description)				\
+	do {								\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
+			exit(EXIT_FAILURE);				\
+		}							\
+	} while(0)
+
+#endif
+
+
 /**
  * Readline from mini-shell.
  */
